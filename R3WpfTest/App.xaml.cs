387b8b36969ece6d06f595c67e8e376b1ca79185
ongaeshi <ongaeshi0621@gmail.com>
2024-03-25 23:55:24 +0900

    プロジェクト ファイルを追加します。

---
+++ R3WpfTest/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace R3WpfTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
