512f97e897e102bffe751638932f1ff904607d2b
ongaeshi <ongaeshi0621@gmail.com>
2024-03-26 00:27:18 +0900

    feat: EveryValueChanged を使ってみる

--- R3WpfTest/App.xaml.cs
+++ R3WpfTest/App.xaml.cs
@@ -1,14 +1,18 @@
-﻿using System.Configuration;
+﻿using R3;
+using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Windows;
 
 namespace R3WpfTest
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // You need to set UnhandledExceptionHandler
+            WpfProviderInitializer.SetDefaultObservableSystem(ex => Trace.WriteLine($"R3 UnhandledException:{ex}"));
+        }
     }
 
 }
--- R3WpfTest/MainWindow.xaml.cs
+++ R3WpfTest/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using R3;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,14 +12,23 @@ using System.Windows.Shapes;
 
 namespace R3WpfTest
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        IDisposable disposable;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            var d1 = Observable.EveryValueChanged(this, x => x.Width).Subscribe(x => WidthText.Text = x.ToString());
+            var d2 = Observable.EveryValueChanged(this, x => x.Height).Subscribe(x => HeightText.Text = x.ToString());
+
+            disposable = Disposable.Combine(d1, d2);
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            disposable.Dispose();
         }
     }
 }
